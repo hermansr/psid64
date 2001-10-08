04582a20dbbdd60db085ad3dace3b57ff97b01e7
Roland Hermans <rolandh@users.sourceforge.net>
2001-10-08 20:57:47 +0000

    remove the accidently added file .o65

--- src/main.c
+++ src/main.c
@@ -33,12 +33,17 @@
 #endif
 
 #include <errno.h>
+#ifdef HAVE_GETOPT_H
+#include <getopt.h>
+#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif
+
+#include "glib.h"
 
 #include "datatypes.h"
 #include "psid.h"
@@ -55,21 +60,26 @@
  *                     L O C A L   D E F I N I T I O N S                    *
  ****************************************************************************/
 
-#define STR_GETOPT_OPTIONS		":hr:vV"
+#define STR_GETOPT_OPTIONS		":ho:r:vV"
 #define EXIT_ERROR			1
 
 typedef struct config_s
 {
     int                     verbose;
     char                   *p_hvsc_root;
+    char                   *p_output_filename;
 }
 config_t;
 
 
 #define NUM_MINDRV_PAGES		2	/* driver without screen display */
 #define NUM_EXTDRV_PAGES		4	/* driver with screen display */
-#define MAX_PAGES			256
+#define NUM_SCREEN_PAGES		4	/* size of screen in pages */
+#define NUM_CHAR_PAGES			8	/* size of charset in pages */
+#define MAX_PAGES			256	/* number of memory pages */
 
+#define PSID_POSTFIX			".sid"
+#define PRG_POSTFIX			".prg"
 
 #define MAX_BLOCKS			3
 
@@ -85,13 +95,20 @@ block_t;
 typedef int             (*SORTFUNC) (const void *, const void *);
 
 
-static void             find_free_space (BYTE * p_driver, BYTE * p_screen);
+static BYTE             find_driver_space (BYTE * pages, BYTE scr, BYTE chars,
+					   BYTE size);
+static void             find_free_space (BYTE * p_driver, BYTE * p_screen,
+					 BYTE * p_chars);
 static void             psid_init_driver (BYTE ** ptr, int *n,
-					  BYTE driver_page, BYTE screen_page);
+					  BYTE driver_page, BYTE screen_page,
+					  BYTE char_page);
 static void             draw_screen (BYTE * p_screen);
 static int              block_cmp (block_t * a, block_t * b);
-static int              process_file (char *p_psid_file, char *p_c64_file, config_t * p_config);
+static char            *build_output_filename (char *p_psid_file,
+					       config_t * p_config);
+static int              process_file (char *p_psid_file, config_t * p_config);
 static void             print_usage (void);
+static void             print_help (void);
 
 
 /****************************************************************************
@@ -105,151 +122,177 @@ static psid_t          *psid;
  *                       L O C A L   F U N C T I O N S                      *
  ****************************************************************************/
 
+static                  BYTE
+find_driver_space (BYTE * pages, BYTE scr, BYTE chars, BYTE size)
+{
+    BYTE                    first_page;
+    int                     i;
+
+    first_page = 0;
+    for (i = 0; i < MAX_PAGES; i++)
+    {
+	if (pages[i] || (scr && (scr <= i) && (i < (scr + NUM_SCREEN_PAGES)))
+	    || (chars && (chars <= i) && (i < (chars + NUM_CHAR_PAGES))))
+	{
+	    if ((i - first_page) >= NUM_EXTDRV_PAGES)
+	    {
+		return first_page;
+	    }
+	    first_page = i + 1;
+	}
+    }
+
+    return 0;
+}
+
+
 static void
-find_free_space (BYTE * p_driver, BYTE * p_screen)
+find_free_space (BYTE * p_driver, BYTE * p_screen, BYTE * p_chars)
 /*--------------------------------------------------------------------------*
    In          : -
    Out         : p_driver		startpage of driver, 0 means no driver
 		 p_screen		startpage of screen, 0 means no screen
+		 p_chars		startpage of chars, 0 means no chars
    Return value: -
    Description : Find free space in the C64 memory map for the screen and the
 		 driver code. Of course the driver code takes priority over
 		 the screen.
    Globals     : psid			PSID header and data
    History     : 15-AUG-2001  RH  Initial version
+		 21-SEP-2001  RH  Added support for screens located in the
+				  memory ranges $4000-$8000 and $c000-$d000.
  *--------------------------------------------------------------------------*/
 {
-    BYTE                    screens[] = {
-	0x04, 0x08, 0x0c,
-	0x20, 0x24, 0x28, 0x2c,
-	0x30, 0x34, 0x38, 0x3c,
-	0x80, 0x84, 0x88, 0x8c,
-	0xa0, 0xa4, 0xa8, 0xac,
-	0xb0, 0xb4, 0xb8, 0xbc,
-	0x00
-    };				/* possible screen locations */
+    BYTE                    pages[MAX_PAGES];
     int                     startp = psid->start_page;
     int                     maxp = psid->max_pages;
+    int                     endp;
     int                     i;
+    int                     j;
+    int                     k;
+    BYTE                    bank;
+    BYTE                    scr;
+    BYTE                    chars;
+    BYTE                    driver;
 
     *p_screen = (BYTE) (0x00);
     *p_driver = (BYTE) (0x00);
+    *p_chars = (BYTE) (0x00);
 
     if (startp == 0x00)
     {
 	/* Used memory ranges. */
-	int                     used[] = { 0x00, 0x03,
+	int                     used[] = {
+	    0x00, 0x03,
 	    0xa0, 0xbf,
 	    0xd0, 0xff,
 	    0x00, 0x00
 	};			/* calculated below */
-	int                     pages[MAX_PAGES];
-	int                     endp;
-	int                     page;
-	int                     last_page;
-
-	startp = psid->load_addr >> 8;
-	endp = (psid->load_addr + psid->data_size - 1) >> 8;
 
 	/* Finish initialization by setting start and end pages. */
-	used[6] = startp;
-	used[7] = endp;
+	used[6] = psid->load_addr >> 8;
+	used[7] = (psid->load_addr + psid->data_size - 1) >> 8;
 
 	/* Mark used pages in table. */
 	memset (pages, 0, sizeof (pages));
 	for (i = 0; i < sizeof (used) / sizeof (*used); i += 2)
 	{
-	    for (page = used[i]; page <= used[i + 1]; page++)
+	    for (j = used[i]; j <= used[i + 1]; j++)
 	    {
-		pages[page] = 1;
-	    }
-	}
-
-	i = 0;
-	while ((*p_screen == 0x00) && (screens[i] != 0x00))
-	{
-	    if ((screens[i] > endp) || ((screens[i] + 4) <= startp))
-	    {
-		/* this screen is availabele, check if driver fits in as well */
-		last_page = 0;
-		page = 0;
-		while ((*p_screen == 0x00) && (page < MAX_PAGES))
-		{
-		    if ((pages[page] != 0)
-			|| ((screens[i] <= page)
-			    && (page < (screens[i] + 4))))
-		    {
-			if ((page - last_page) >= NUM_EXTDRV_PAGES)
-			{
-			    *p_screen = screens[i];
-			    *p_driver = last_page;
-			}
-			last_page = page + 1;
-		    }
-		    page++;
-		}
-	    }
-	    i++;
-	}
-
-	if (*p_screen == 0x00)
-	{
-	    /* no suitable screen location found, but there might be enough
-	       room for the driver code. */
-	    last_page = 0;
-	    page = 0;
-	    while ((*p_driver == 0x00) && (page < MAX_PAGES))
-	    {
-		if (pages[page] != 0)
-		{
-		    if ((page - last_page) >= NUM_MINDRV_PAGES)
-		    {
-			*p_driver = last_page;
-		    }
-		    last_page = page + 1;
-		}
-		page++;
+		pages[j] = 1;
 	    }
 	}
     }
     else if (startp != 0xff)
     {
 	/* the available pages have been specified in the PSID file */
-	i = 0;
-	while ((*p_screen == 0x00) && (screens[i] != 0x00))
+	endp = MIN ((startp + maxp), MAX_PAGES);
+	for (i = 0; i < MAX_PAGES; i++)
 	{
-	    /* check if screens[i] fits into the specified area */
-	    if ((startp <= screens[i])
-		&& ((screens[i] + 4) <= (startp + maxp)))
+	    pages[i] = ((startp <= i) && (i < endp)) ? 0 : 1;
+	}
+    }
+    else
+    {
+	/* not a single page is available */
+	return;
+    }
+
+    driver = 0;
+    for (i = 0; i < 4; i++)
+    {
+	/* Calculate the VIC bank offset. Screens located inside banks 1 and 3
+	   require a copy the character rom in ram. The code below uses a
+	   little trick to swap bank 1 and 2 so that bank 0 and 2 are checked
+	   before 1 and 3. */
+	bank = (((i & 1) ^ (i >> 1)) ? i ^ 3 : i) << 6;
+
+	for (j = 0; j < 0x40; j += 4)
+	{
+	    /* screen may not reside within the char rom mirror areas */
+	    if (!(bank & 0x40) && (0x10 <= j) && (j < 0x20))
+		continue;
+
+	    /* check if screen area is available */
+	    scr = bank + j;
+	    if (pages[scr] || pages[scr + 1] || pages[scr + 2]
+		|| pages[scr + 3])
+		continue;
+
+	    if (bank & 0x40)
 	    {
-		/* this screen is availabele, check if driver fits in as well */
-		if ((screens[i] - startp) >= NUM_EXTDRV_PAGES)
+		/* The char rom needs to be copied to RAM so let's try to find
+		   a suitable location. */
+		for (k = 0; k < 0x40; k += 8)
 		{
-		    *p_screen = screens[i];
-		    *p_driver = startp;
-		}
-		else if ((startp + maxp - (screens[i] + 4)) >=
-			 NUM_EXTDRV_PAGES)
-		{
-		    *p_screen = screens[i];
-		    *p_driver = screens[i] + 4;
+		    /* char rom area may not overlap with screen area */
+		    if (k == (j & 0x38))
+			continue;
+
+		    /* check if character rom area is avaiable */
+		    chars = bank + k;
+		    if (pages[chars] || pages[chars + 1]
+			|| pages[chars + 2] || pages[chars + 3]
+			|| pages[chars + 4] || pages[chars + 5]
+			|| pages[chars + 6] || pages[chars + 7])
+			continue;
+
+		    driver =
+			find_driver_space (pages, scr, chars,
+					   NUM_EXTDRV_PAGES);
+		    if (driver)
+		    {
+			*p_driver = driver;
+			*p_screen = scr;
+			*p_chars = chars;
+			return;
+		    }
 		}
 	    }
-	    i++;
+	    else
+	    {
+		driver = find_driver_space (pages, scr, 0, NUM_EXTDRV_PAGES);
+		if (driver)
+		{
+		    *p_driver = driver;
+		    *p_screen = scr;
+		    return;
+		}
+	    }
 	}
+    }
 
-	if ((*p_screen == 0x00) && (maxp >= NUM_MINDRV_PAGES))
-	{
-	    /* no suitable screen location found, but there is enough room
-	       for the driver code. */
-	    *p_driver = startp;
-	}
+    if (!driver)
+    {
+	driver = find_driver_space (pages, 0, 0, NUM_MINDRV_PAGES);
+	*p_driver = driver;
     }
 }
 
 
 static void
-psid_init_driver (BYTE ** ptr, int *n, BYTE driver_page, BYTE screen_page)
+psid_init_driver (BYTE ** ptr, int *n, BYTE driver_page, BYTE screen_page,
+		  BYTE char_page)
 {
     BYTE                    psid_driver[] = {
 #include "psiddrv.h"
@@ -262,6 +305,8 @@ psid_init_driver (BYTE ** ptr, int *n, BYTE driver_page, BYTE screen_page)
     int                     psid_size;
     ADDRESS                 reloc_addr;
     ADDRESS                 addr;
+    BYTE                    vsa;	/* video screen address */
+    BYTE                    cba;	/* character memory base address */
 
     *ptr = NULL;
     *n = 0;
@@ -292,7 +337,7 @@ psid_init_driver (BYTE ** ptr, int *n, BYTE driver_page, BYTE screen_page)
 
     if (!reloc65 ((char **) &psid_reloc, &psid_size, reloc_addr))
     {
-	fprintf (stderr, "Relocation error.");
+	fprintf (stderr, "%s: Relocation error.", PACKAGE);
 	return;
     }
 
@@ -316,7 +361,9 @@ psid_init_driver (BYTE ** ptr, int *n, BYTE driver_page, BYTE screen_page)
     if (screen_page != 0x00)
     {
 	psid_reloc[addr++] = (BYTE) ((((screen_page & 0xc0) >> 6) ^ 3) | 0x04);	/* dd00 */
-	psid_reloc[addr++] = (BYTE) (((screen_page & 0x3c) << 2) | 0x06);	/* d018 */
+	vsa = (BYTE) ((screen_page & 0x3c) << 2);
+	cba = (BYTE) (char_page ? (char_page >> 2) & 0x0e : 0x06);
+	psid_reloc[addr++] = vsa | cba;	/* d018 */
     }
 
     *ptr = psid_reloc;
@@ -333,7 +380,8 @@ draw_screen (BYTE * p_screen)
     screen_clear (p_screen);
 
     /* set title */
-    screen_printf (p_screen, 5, 1, "PSID64 v%3.3s by Roland Hermans!", VERSION);
+    screen_printf (p_screen, 5, 1, "PSID64 v%3.3s by Roland Hermans!",
+		   VERSION);
 
     /* characters for color line effect */
     p_screen[4] = 0x70;
@@ -353,7 +401,7 @@ draw_screen (BYTE * p_screen)
     screen_printf (p_screen, 0, 5, "Author   : %-29.29s", psid->author);
     screen_printf (p_screen, 0, 6, "Copyright: %-29.29s", psid->copyright);
     screen_printf (p_screen, 0, 7, "Load     : $%04X-$%04X",
-	     psid->load_addr, psid->load_addr + psid->data_size);
+		   psid->load_addr, psid->load_addr + psid->data_size);
     screen_printf (p_screen, 0, 8, "Init     : $%04X", psid->init_addr);
     if (psid->play_addr)
     {
@@ -404,10 +452,45 @@ block_cmp (block_t * a, block_t * b)
 }
 
 
-static int
-process_file (char *p_psid_file, char *p_c64_file, config_t * p_config)
+static char            *
+build_output_filename (char *p_psid_file, config_t * p_config)
 {
-    BYTE                    screen[SCREEN_SIZE];
+    char                   *p_output_filename = NULL;
+    char                   *p_str;
+    int                     l;
+    int                     n;
+
+    if (p_config->p_output_filename != NULL)
+    {
+	p_output_filename = g_strdup (p_config->p_output_filename);
+    }
+    else
+    {
+	p_str = g_basename (p_psid_file);
+	l = (p_str != NULL) ? strlen (p_str) : 0;
+	n = strlen (PSID_POSTFIX);
+	if ((l >= n) && (g_strcasecmp (p_str + l - n, PSID_POSTFIX) == 0))
+	{
+	    p_str = g_strndup (p_str, l - n);
+	}
+	else
+	{
+	    p_str = g_strdup (p_str);
+	}
+	p_output_filename = g_strdup_printf ("%s%s", p_str, PRG_POSTFIX);
+	g_free (p_str);
+    }
+
+    return p_output_filename;
+}
+
+
+static int
+process_file (char *p_psid_file, config_t * p_config)
+{
+    int                     retval = 0;
+    char                   *p_output_filename;
+     /*FIXME*/ BYTE screen[SCREEN_SIZE];
     block_t                 blocks[MAX_BLOCKS];
     int                     n_blocks;
     int                     i;
@@ -422,30 +505,35 @@ process_file (char *p_psid_file, char *p_c64_file, config_t * p_config)
     ADDRESS                 addr;
     ADDRESS                 eof;
     ADDRESS                 size;
-    BYTE                    screen_page;
     BYTE                    driver_page;
+    BYTE                    screen_page;
+    BYTE                    char_page;
 
     /* read the PSID file */
     if (p_config->verbose)
     {
-	printf ("Reading PSID file '%s'\n", p_psid_file);
+	printf ("Reading PSID file `%s'\n", p_psid_file);
     }
     psid = psid_load_file (p_psid_file);
     if (!psid)
     {
+	fprintf (stderr, "%s: `%s' is not a valid PSID file.\n", PACKAGE,
+		 p_psid_file);
 	return 1;
     }
 
     /* find space for driver and screen (optional) */
-    find_free_space (&driver_page, &screen_page);
+    find_free_space (&driver_page, &screen_page, &char_page);
     if (driver_page == 0x00)
     {
-	fprintf (stderr, "C64 memory has no space for driver code.\n");
+	fprintf (stderr, "%s: C64 memory has no space for driver code.\n",
+		 p_psid_file);
 	return 1;
     }
 
     /* relocate and initialize the driver */
-    psid_init_driver (&psid_driver, &driver_size, driver_page, screen_page);
+    psid_init_driver (&psid_driver, &driver_size, driver_page, screen_page,
+		      char_page);
     jmp_addr = driver_page << 8;
 
     /* fill the blocks structure */
@@ -474,11 +562,24 @@ process_file (char *p_psid_file, char *p_c64_file, config_t * p_config)
     /* print memory map */
     if (p_config->verbose)
     {
+	ADDRESS                 charset = char_page << 8;
+
 	printf ("C64 memory map:\n");
 	for (i = n_blocks - 1; i >= 0; i--)
 	{
+	    if ((charset != 0) && (blocks[i].load > charset))
+	    {
+		printf ("  $%04x-$%04x  Character set\n", charset,
+			charset + (256 * NUM_CHAR_PAGES));
+		charset = 0;
+	    }
 	    printf ("  $%04x-$%04x  %s\n", blocks[i].load,
 		    blocks[i].load + blocks[i].size, blocks[i].p_description);
+	}
+	if (charset != 0)
+	{
+	    printf ("  $%04x-$%04x  Character set\n", charset,
+		    charset + (256 * NUM_CHAR_PAGES));
 	}
     }
 
@@ -510,16 +611,18 @@ process_file (char *p_psid_file, char *p_c64_file, config_t * p_config)
     }
     addr = addr + 4 * MAX_BLOCKS;
 
+    psid_boot[addr++] = (BYTE) (char_page);	/* page for character set, or 0 */
     psid_boot[addr++] = (BYTE) (jmp_addr & 0xff);	/* start address of driver */
     psid_boot[addr++] = (BYTE) (jmp_addr >> 8);
 
     /* write C64 executable */
-    f = fopen (p_c64_file, "w");
+    p_output_filename = build_output_filename (p_psid_file, p_config);
+    f = fopen (p_output_filename, "w");
     if (f != NULL)
     {
 	if (p_config->verbose)
 	{
-	    printf ("Writing C64 executable '%s'\n", p_c64_file);
+	    printf ("Writing C64 executable `%s'\n", p_output_filename);
 	}
 	fwrite (psid_boot, boot_size, 1, f);
 	for (i = n_blocks - 1; i >= 0; i--)
@@ -529,12 +632,13 @@ process_file (char *p_psid_file, char *p_c64_file, config_t * p_config)
     }
     else
     {
-	fprintf (stderr, "could not open file %s: %s", p_c64_file,
-		 strerror (errno));
-	return 1;
+	fprintf (stderr, "%s: can't open output file `%s': %s\n", PACKAGE,
+		 p_output_filename, g_strerror (errno));
+	retval = 1;
     }
+    g_free (p_output_filename);
 
-    return 0;
+    return retval;
 }
 
 
@@ -543,6 +647,28 @@ print_usage (void)
 {
     printf ("Usage: %s [-hvV] [-r hvsc_root] <psid_file> <c64_file>\n",
 	    PACKAGE);
+}
+
+
+static void
+print_help (void)
+{
+    print_usage ();
+    printf ("\n");
+#ifdef HAVE_GETOPT_LONG
+    printf ("  -o, --output=FILE    specify output file\n");
+    printf ("  -r, --root           specify HVSC root directory\n");
+    printf ("  -v, --verbose        explain what is being done\n");
+    printf ("  -h, --help           display this help and exit\n");
+    printf ("  -V, --version        output version information and exit\n");
+#else
+    printf ("  -o                   specify output file\n");
+    printf ("  -r                   specify HVSC root directory\n");
+    printf ("  -v                   explain what is being done\n");
+    printf ("  -h                   display this help and exit\n");
+    printf ("  -V                   output version information and exit\n");
+#endif
+    printf ("\nReport bugs to <rolandh@users.sourceforge.net>.\n");
 }
 
 
@@ -556,19 +682,39 @@ main (int argc, char **argv)
     config_t                config;
     int                     c;
     int                     errflg = 0;
+#ifdef HAVE_GETOPT_LONG
+    int                     option_index = 0;
+    static struct option    long_options[] = {
+	{"help", 0, NULL, 'h'},
+	{"output", 1, NULL, 'o'},
+	{"root", 1, NULL, 'r'},
+	{"verbose", 0, NULL, 'v'},
+	{"version", 0, NULL, 'V'},
+	{NULL, 0, NULL, 0}
+    };
+#endif
 
     /* set default configuration */
     config.verbose = 0;
     config.p_hvsc_root = NULL;
+    config.p_output_filename = NULL;
 
+#ifdef HAVE_GETOPT_LONG
+    c = getopt_long (argc, argv, STR_GETOPT_OPTIONS, long_options,
+		     &option_index);
+#else
     c = getopt (argc, argv, STR_GETOPT_OPTIONS);
+#endif
     while (c != -1)
     {
 	switch (c)
 	{
 	case 'h':
-	    print_usage ();
+	    print_help ();
 	    exit (0);
+	    break;
+	case 'o':
+	    config.p_output_filename = optarg;
 	    break;
 	case 'r':
 	    config.p_hvsc_root = optarg;
@@ -581,31 +727,47 @@ main (int argc, char **argv)
 	    exit (0);
 	    break;
 	case ':':
-	    fprintf (stderr, "Option -%c requires an argument\n", optopt);
+	    fprintf (stderr, "%s: option requires an argument -- %c\n",
+		     PACKAGE, optopt);
 	    errflg++;
 	    break;
 	case '?':
-	    fprintf (stderr, "Unrecognized option: - %c\n", optopt);
+	    fprintf (stderr, "%s: invalid option -- %c\n", PACKAGE, optopt);
 	    errflg++;
 	    break;
 	default:
-	    fprintf (stderr, "%s:%d: software error\n", __FILE__, __LINE__);
+	    fprintf (stderr, "%s:%s:%d: software error\n", PACKAGE, __FILE__,
+		     __LINE__);
 	    exit (EXIT_ERROR);
 	    break;
 	}
+#ifdef HAVE_GETOPT_LONG
+	c = getopt_long (argc, argv, STR_GETOPT_OPTIONS, long_options,
+			 &option_index);
+#else
 	c = getopt (argc, argv, STR_GETOPT_OPTIONS);
+#endif
     }
 
-    if ((argc - optind) != 2)
+    if ((argc - optind) < 1)
     {
+	print_usage ();
 	errflg++;
     }
 
     if (errflg)
     {
-	print_usage ();
+	fprintf (stderr, "Try `%s --help' for more information.\n", PACKAGE);
 	exit (EXIT_ERROR);
     }
 
-    return process_file (argv[optind], argv[optind + 1], &config);
+    while (optind < argc)
+    {
+	if (process_file (argv[optind++], &config))
+	{
+	    exit (EXIT_ERROR);
+	}
+    }
+
+    return 0;
 }
