hermansr/psid64

commit e0fec3a71aab97ece7aaea45996f89608da6db9d

Add SPDX license identifiers

src/libpsid64/Makefile.am
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+
 EXTRA_DIST = stilview/README.txt stilview/STILView-config.patch
 
 lib_LIBRARIES = libpsid64.a

src/libpsid64/psidboot.h
@@ -17,9 +17,5 @@
 0x29,0x03,0xa0,0x73,0xb9,0x04,0x10,0x99,0xf7,0x00,0x88,0xd0,0xf7,0x4c,0x14,0x01,
 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
 0x00,0x6b,0x37,0x00,0x00,0x00,0x08,0x00,0x14,0x0f,0x00,0x00,0x00,0x00,0x00,0x00,
-0x00,0x06,0x01,0x02,0x03,0x04,0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x04,0x00,
-0x73,0x6f,0x6e,0x67,0x00,0x70,0x6c,0x61,0x79,0x65,0x72,0x00,0x73,0x74,0x6f,0x70,
-0x76,0x65,0x63,0x00,0x43,0x4f,0x4c,0x5f,0x42,0x4f,0x52,0x44,0x45,0x52,0x00,0x02,
-0x20,0x00,0x00,0x02,0x82,0x73,0x80,0x01,0x00,0x1b,0x82,0x0d,0x20,0x02,0x00,0x05,
-0x40,0x02,0x00,0x00,0x07,0x82,0x2b,0x20,0x03,0x00,0x00,0x00,0x00,0x00,
+0x00,0x06,0x01,0x02,0x03,0x04,0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,
 

src/libpsid64/psiddrv.h
@@ -33,11 +33,5 @@
 0xf7,0xef,0xef,0xef,0xdf,0xef,0xef,0xef,0xdf,0x7f,0xfb,0xfd,0xfb,0xf7,0xf7,0xfd,
 0xfb,0xf7,0xfd,0x80,0x01,0x08,0x01,0x08,0x01,0x08,0x01,0x08,0x01,0x08,0x04,0x10,
 0x10,0x04,0x40,0x20,0x04,0x20,0x02,0x04,0x20,0x04,0x10,0x80,0x40,0x02,0x40,0x02,
-0x20,0x40,0x40,0x80,0x02,0x80,0x02,0x10,0x01,0x00,0x73,0x69,0x64,0x32,0x62,0x61,
-0x73,0x65,0x00,0x02,0x82,0x03,0x82,0x13,0x82,0x02,0x82,0x02,0x82,0x0a,0x82,0x07,
-0x82,0x12,0x82,0x17,0x82,0x04,0x82,0x04,0x82,0x14,0x82,0x03,0x82,0x05,0x82,0x13,
-0x82,0x15,0x82,0x03,0x82,0x09,0x82,0x17,0x82,0x16,0x82,0x05,0x22,0x09,0x82,0x0b,
-0x82,0x08,0x82,0x03,0x82,0x05,0x82,0x0f,0x82,0x05,0x82,0x04,0x82,0x39,0x80,0x00,
-0x00,0x15,0x82,0x05,0x82,0x09,0x82,0x05,0x82,0x0a,0x82,0x09,0x82,0x08,0x82,0x04,
-0x82,0x06,0x82,0x05,0x82,0x00,0x00,0x00,0x00,
+0x20,0x40,0x40,0x80,0x02,0x80,0x02,0x10,
 

src/libpsid64/psidextboot.h
@@ -27,33 +27,5 @@
 0x00,0x00,0x00,0x00,0x00,0x00,0x6b,0x37,0x00,0x00,0x01,0x08,0x00,0x00,0x0f,0x00,
 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x01,0x02,0x03,0x04,0x00,0x00,0x02,0x03,0x04,
 0x05,0x06,0x07,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
-0x00,0x00,0x1e,0x00,0x73,0x6f,0x6e,0x67,0x00,0x62,0x61,0x72,0x73,0x70,0x72,0x70,
-0x74,0x72,0x00,0x73,0x63,0x72,0x65,0x65,0x6e,0x00,0x70,0x6c,0x61,0x79,0x65,0x72,
-0x00,0x64,0x64,0x30,0x30,0x00,0x43,0x4f,0x4c,0x5f,0x42,0x41,0x43,0x4b,0x47,0x52,
-0x4f,0x55,0x4e,0x44,0x00,0x43,0x4f,0x4c,0x5f,0x54,0x49,0x54,0x4c,0x45,0x00,0x43,
-0x4f,0x4c,0x5f,0x56,0x41,0x4c,0x55,0x45,0x00,0x43,0x4f,0x4c,0x5f,0x50,0x41,0x52,
-0x41,0x4d,0x45,0x54,0x45,0x52,0x00,0x43,0x4f,0x4c,0x5f,0x43,0x4f,0x4c,0x4f,0x4e,
-0x00,0x43,0x4f,0x4c,0x5f,0x4c,0x45,0x47,0x45,0x4e,0x44,0x00,0x73,0x74,0x6f,0x70,
-0x76,0x65,0x63,0x00,0x64,0x30,0x31,0x38,0x00,0x43,0x4f,0x4c,0x5f,0x42,0x4f,0x52,
-0x44,0x45,0x52,0x00,0x43,0x4f,0x4c,0x5f,0x42,0x41,0x52,0x5f,0x46,0x47,0x00,0x43,
-0x4f,0x4c,0x5f,0x4c,0x49,0x4e,0x45,0x5f,0x30,0x00,0x43,0x4f,0x4c,0x5f,0x4c,0x49,
-0x4e,0x45,0x5f,0x31,0x00,0x43,0x4f,0x4c,0x5f,0x4c,0x49,0x4e,0x45,0x5f,0x32,0x00,
-0x43,0x4f,0x4c,0x5f,0x4c,0x49,0x4e,0x45,0x5f,0x33,0x00,0x43,0x4f,0x4c,0x5f,0x4c,
-0x49,0x4e,0x45,0x5f,0x34,0x00,0x43,0x4f,0x4c,0x5f,0x4c,0x49,0x4e,0x45,0x5f,0x35,
-0x00,0x43,0x4f,0x4c,0x5f,0x4c,0x49,0x4e,0x45,0x5f,0x36,0x00,0x43,0x4f,0x4c,0x5f,
-0x4c,0x49,0x4e,0x45,0x5f,0x37,0x00,0x43,0x4f,0x4c,0x5f,0x4c,0x49,0x4e,0x45,0x5f,
-0x38,0x00,0x43,0x4f,0x4c,0x5f,0x4c,0x49,0x4e,0x45,0x5f,0x39,0x00,0x43,0x4f,0x4c,
-0x5f,0x4c,0x49,0x4e,0x45,0x5f,0x31,0x30,0x00,0x43,0x4f,0x4c,0x5f,0x4c,0x49,0x4e,
-0x45,0x5f,0x31,0x31,0x00,0x43,0x4f,0x4c,0x5f,0x4c,0x49,0x4e,0x45,0x5f,0x31,0x32,
-0x00,0x43,0x4f,0x4c,0x5f,0x4c,0x49,0x4e,0x45,0x5f,0x31,0x33,0x00,0x43,0x4f,0x4c,
-0x5f,0x4c,0x49,0x4e,0x45,0x5f,0x31,0x34,0x00,0x02,0x20,0x00,0x00,0x02,0x82,0x9c,
-0x20,0x01,0x00,0x02,0x80,0x02,0x00,0x07,0x80,0x03,0x00,0x1b,0x82,0x09,0x20,0x04,
-0x00,0x05,0x20,0x05,0x00,0x13,0x20,0x06,0x00,0x0a,0x82,0x0d,0x20,0x07,0x00,0x08,
-0x20,0x08,0x00,0x03,0x20,0x09,0x00,0x0f,0x20,0x0a,0x00,0x0e,0x20,0x0b,0x00,0x05,
-0x40,0x0b,0x00,0x00,0x07,0x82,0x23,0x20,0x0c,0x00,0x08,0x20,0x0d,0x00,0x01,0x20,
-0x05,0x00,0x06,0x20,0x0e,0x00,0x07,0x20,0x0f,0x00,0x01,0x20,0x10,0x00,0x01,0x20,
-0x11,0x00,0x01,0x20,0x12,0x00,0x01,0x20,0x13,0x00,0x01,0x20,0x14,0x00,0x01,0x20,
-0x15,0x00,0x01,0x20,0x16,0x00,0x01,0x20,0x17,0x00,0x01,0x20,0x18,0x00,0x01,0x20,
-0x19,0x00,0x01,0x20,0x1a,0x00,0x01,0x20,0x1b,0x00,0x01,0x20,0x1c,0x00,0x01,0x20,
-0x1d,0x00,0x00,0x00,0x00,0x00,
+0x00,0x00,
 

src/libpsid64/psidextdrv.h
@@ -82,62 +82,4 @@
 0xbd,0x49,0x03,0x9d,0x48,0x03,0xe8,0xe0,0x27,0x90,0xf5,0xae,0xda,0x14,0xe0,0xff,
 0xd0,0x09,0xe8,0x8e,0xd7,0x14,0x8c,0xd8,0x14,0xf0,0xf0,0x8e,0x6f,0x03,0xee,0xd7,
 0x14,0xd0,0x03,0xee,0xd8,0x14,0x60,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
-0x24,0x00,0x43,0x4f,0x4c,0x5f,0x52,0x41,0x53,0x54,0x45,0x52,0x5f,0x54,0x49,0x4d,
-0x45,0x00,0x73,0x63,0x72,0x65,0x65,0x6e,0x5f,0x73,0x6f,0x6e,0x67,0x6e,0x75,0x6d,
-0x00,0x73,0x63,0x72,0x65,0x65,0x6e,0x00,0x73,0x6f,0x6e,0x67,0x6c,0x65,0x6e,0x67,
-0x74,0x68,0x73,0x5f,0x6d,0x69,0x6e,0x00,0x73,0x6f,0x6e,0x67,0x6c,0x65,0x6e,0x67,
-0x74,0x68,0x73,0x5f,0x73,0x65,0x63,0x00,0x73,0x6f,0x6e,0x67,0x74,0x70,0x69,0x5f,
-0x6c,0x6f,0x00,0x73,0x6f,0x6e,0x67,0x74,0x70,0x69,0x5f,0x68,0x69,0x00,0x73,0x69,
-0x64,0x32,0x62,0x61,0x73,0x65,0x00,0x73,0x69,0x64,0x33,0x62,0x61,0x73,0x65,0x00,
-0x43,0x4f,0x4c,0x5f,0x42,0x41,0x52,0x5f,0x46,0x47,0x00,0x43,0x4f,0x4c,0x5f,0x42,
-0x41,0x52,0x5f,0x42,0x47,0x00,0x43,0x4f,0x4c,0x5f,0x46,0x4f,0x4f,0x54,0x45,0x52,
-0x5f,0x30,0x00,0x43,0x4f,0x4c,0x5f,0x46,0x4f,0x4f,0x54,0x45,0x52,0x5f,0x31,0x00,
-0x43,0x4f,0x4c,0x5f,0x46,0x4f,0x4f,0x54,0x45,0x52,0x5f,0x32,0x00,0x43,0x4f,0x4c,
-0x5f,0x46,0x4f,0x4f,0x54,0x45,0x52,0x5f,0x33,0x00,0x43,0x4f,0x4c,0x5f,0x46,0x4f,
-0x4f,0x54,0x45,0x52,0x5f,0x34,0x00,0x43,0x4f,0x4c,0x5f,0x46,0x4f,0x4f,0x54,0x45,
-0x52,0x5f,0x35,0x00,0x43,0x4f,0x4c,0x5f,0x46,0x4f,0x4f,0x54,0x45,0x52,0x5f,0x36,
-0x00,0x43,0x4f,0x4c,0x5f,0x46,0x4f,0x4f,0x54,0x45,0x52,0x5f,0x37,0x00,0x43,0x4f,
-0x4c,0x5f,0x46,0x4f,0x4f,0x54,0x45,0x52,0x5f,0x38,0x00,0x43,0x4f,0x4c,0x5f,0x46,
-0x4f,0x4f,0x54,0x45,0x52,0x5f,0x39,0x00,0x43,0x4f,0x4c,0x5f,0x46,0x4f,0x4f,0x54,
-0x45,0x52,0x5f,0x31,0x30,0x00,0x43,0x4f,0x4c,0x5f,0x46,0x4f,0x4f,0x54,0x45,0x52,
-0x5f,0x31,0x31,0x00,0x43,0x4f,0x4c,0x5f,0x46,0x4f,0x4f,0x54,0x45,0x52,0x5f,0x31,
-0x32,0x00,0x43,0x4f,0x4c,0x5f,0x46,0x4f,0x4f,0x54,0x45,0x52,0x5f,0x31,0x33,0x00,
-0x43,0x4f,0x4c,0x5f,0x46,0x4f,0x4f,0x54,0x45,0x52,0x5f,0x31,0x34,0x00,0x43,0x4f,
-0x4c,0x5f,0x46,0x4f,0x4f,0x54,0x45,0x52,0x5f,0x31,0x35,0x00,0x73,0x74,0x69,0x6c,
-0x00,0x43,0x4f,0x4c,0x5f,0x53,0x43,0x52,0x4f,0x4c,0x4c,0x45,0x52,0x5f,0x30,0x00,
-0x43,0x4f,0x4c,0x5f,0x53,0x43,0x52,0x4f,0x4c,0x4c,0x45,0x52,0x5f,0x31,0x00,0x43,
-0x4f,0x4c,0x5f,0x53,0x43,0x52,0x4f,0x4c,0x4c,0x45,0x52,0x5f,0x32,0x00,0x43,0x4f,
-0x4c,0x5f,0x53,0x43,0x52,0x4f,0x4c,0x4c,0x45,0x52,0x5f,0x33,0x00,0x43,0x4f,0x4c,
-0x5f,0x53,0x43,0x52,0x4f,0x4c,0x4c,0x45,0x52,0x5f,0x34,0x00,0x43,0x4f,0x4c,0x5f,
-0x53,0x43,0x52,0x4f,0x4c,0x4c,0x45,0x52,0x5f,0x35,0x00,0x43,0x4f,0x4c,0x5f,0x53,
-0x43,0x52,0x4f,0x4c,0x4c,0x45,0x52,0x5f,0x36,0x00,0x43,0x4f,0x4c,0x5f,0x53,0x43,
-0x52,0x4f,0x4c,0x4c,0x45,0x52,0x5f,0x37,0x00,0x02,0x82,0x03,0x82,0x20,0x82,0x02,
-0x82,0x02,0x82,0x15,0x20,0x00,0x00,0x08,0x82,0x07,0x82,0x21,0x82,0x03,0x82,0x20,
-0x82,0x04,0x82,0x04,0x82,0x14,0x82,0x03,0x40,0x01,0x00,0x00,0x04,0x82,0x14,0x80,
-0x01,0x00,0x15,0x80,0x01,0x00,0x07,0x80,0x01,0x00,0x05,0x80,0x01,0x00,0x0e,0x82,
-0x03,0x80,0x02,0x00,0x06,0x82,0x03,0x82,0x03,0x82,0x03,0x82,0x03,0x82,0x03,0x82,
-0x05,0x82,0x09,0x82,0x0b,0x82,0x15,0x82,0x03,0x82,0x09,0x40,0x03,0x00,0x00,0x04,
-0x80,0x03,0x00,0x05,0x82,0x03,0x80,0x04,0x00,0x04,0x82,0x03,0x80,0x05,0x00,0x03,
-0x82,0x03,0x80,0x06,0x00,0x03,0x82,0x03,0x82,0x17,0x82,0x16,0x82,0x05,0x22,0x09,
-0x82,0x0b,0x82,0x08,0x82,0x03,0x82,0x05,0x82,0x0f,0x82,0x05,0x82,0x03,0x82,0x1a,
-0x82,0x0e,0x82,0x07,0x82,0x03,0x82,0x03,0x82,0x03,0x82,0x03,0x82,0x2f,0x80,0x07,
-0x00,0x03,0x80,0x08,0x00,0x12,0x80,0x07,0x00,0x03,0x80,0x08,0x00,0x06,0x82,0x08,
-0x80,0x07,0x00,0x03,0x80,0x08,0x00,0x04,0x82,0x08,0x82,0x1a,0x82,0x05,0x82,0x09,
-0x82,0x05,0x82,0x0d,0x82,0x09,0x82,0x08,0x82,0x04,0x82,0x06,0x82,0x06,0x82,0x05,
-0x82,0x0a,0x82,0x04,0x82,0x0c,0x82,0x05,0x82,0x04,0x82,0x05,0x82,0x5d,0x82,0x03,
-0x82,0x03,0x82,0x06,0x82,0x04,0x82,0x0c,0x82,0x05,0x82,0x05,0x82,0x06,0x82,0x06,
-0x82,0x04,0x82,0x07,0x82,0x03,0x82,0x07,0x82,0x03,0x80,0x02,0x00,0x05,0x82,0x0a,
-0x80,0x02,0x00,0x06,0x82,0x03,0x82,0x03,0x82,0x03,0x82,0x04,0x82,0x05,0x82,0x05,
-0x82,0x03,0x82,0x05,0x82,0x03,0x82,0x05,0x82,0x03,0x82,0x03,0x82,0x07,0x82,0x06,
-0x82,0x11,0x82,0x08,0x80,0x02,0x00,0x05,0x82,0x16,0x80,0x02,0x00,0x05,0x80,0x02,
-0x00,0x0d,0x20,0x09,0x00,0x05,0x20,0x0a,0x00,0x3c,0x82,0x03,0x82,0x07,0x82,0x0c,
-0x20,0x0b,0x00,0x01,0x20,0x0c,0x00,0x01,0x20,0x0d,0x00,0x01,0x20,0x0e,0x00,0x01,
-0x20,0x0f,0x00,0x01,0x20,0x10,0x00,0x01,0x20,0x11,0x00,0x01,0x20,0x12,0x00,0x01,
-0x20,0x13,0x00,0x01,0x20,0x14,0x00,0x01,0x20,0x15,0x00,0x01,0x20,0x16,0x00,0x01,
-0x20,0x17,0x00,0x01,0x20,0x18,0x00,0x01,0x20,0x19,0x00,0x01,0x20,0x1a,0x00,0x02,
-0x40,0x1b,0x00,0x00,0x0f,0x82,0x03,0x82,0x07,0x82,0x03,0x82,0x03,0x82,0x06,0x82,
-0x08,0x80,0x02,0x00,0x03,0x80,0x02,0x00,0x08,0x82,0x08,0x82,0x03,0x82,0x05,0x80,
-0x02,0x00,0x03,0x82,0x05,0x82,0x04,0x20,0x1c,0x00,0x01,0x20,0x1d,0x00,0x01,0x20,
-0x1e,0x00,0x01,0x20,0x1f,0x00,0x01,0x20,0x20,0x00,0x01,0x20,0x21,0x00,0x01,0x20,
-0x22,0x00,0x01,0x20,0x23,0x00,0x00,0x00,0x00,0x00,
 

Makefile.am
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+
 # Process this file with automake to produce Makefile.in
 
 SUBDIRS = src

src/Makefile.am
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+
 # Process this file with automake to produce Makefile.in
 
 SUBDIRS = libpsid64 sidtune sidutils

src/sidtune/Makefile.am
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+
 noinst_LIBRARIES = libsidtune.a
 
 libsidtune_a_CFLAGS = $(AM_CFLAGS)

commit 8fec44fef5335e3c3a192274349d5208bf247035

Fix assembler error introduced in e0fec3a
Pass option -M to xa to allow colons to appear in comments. Stop build when XA assembler reports an error. Do not include empty lines in generated header files. Use macro in Automake.am to prevent duplicate code.

By default XA does not allow colons in comments. This resulted in assembler errors due to the added SPDX identifiers. Because of the make recipe not stopping on an assembler error, incomplete and invalid driver headers were created.

src/libpsid64/Makefile.am
@@ -51,20 +51,29 @@ BUILT_SOURCES = \
 	psiddrv.h \
 	psidextdrv.h
 
+define my_xa_recipe
+set -o pipefail ; \
+if xa -M -c -R -G $< -o - $(1) | \
+od -v -An -tx1 | \
+sed -re 's/[[:alnum:]]+/0x&,/g' -e 's/[[:space:]]+//g' -e '/^$$/d' > $(srcdir)/$@ ; \
+then true ; \
+else status=$$? ; $(RM) $(srcdir)/$@ ; exit $$status ; \
+fi
+endef
+
 SUFFIXES = .a65
 
 .h : .a65
-	xa -R -G $< -o $(srcdir)/$*.o65
-	od -v -An -w8 -tx1 $(srcdir)/$*.o65 | sed -re 's/[[:alnum:]]+/0x&,/g' > $(srcdir)/$@
+	$(call my_xa_recipe,)
 
 psidboot.h: psidboot.a65
-	xa -c -R -G $< -o - -DSMOOTH_SCROLL | od -v -An -tx1 | sed -re 's/[[:alnum:]]+/0x&,/g' -e 's/[[:space:]]+//g' > $(srcdir)/$@
+	$(call my_xa_recipe,)
 
 psidextboot.h: psidboot.a65
-	xa -c -R -G $< -o - -DSCREEN -DSMOOTH_SCROLL | od -v -An -tx1 | sed -re 's/[[:alnum:]]+/0x&,/g' -e 's/[[:space:]]+//g' > $(srcdir)/$@
+	$(call my_xa_recipe,-DSCREEN -DSMOOTH_SCROLL)
 
 psiddrv.h: psiddrv.a65
-	xa -c -R -G $< -o - | od -v -An -tx1 | sed -re 's/[[:alnum:]]+/0x&,/g' -e 's/[[:space:]]+//g' > $(srcdir)/$@
+	$(call my_xa_recipe,)
 
 psidextdrv.h: psiddrv.a65
-	xa -c -R -G $< -o - -DSCREEN -DSMOOTH_SCROLL | od -v -An -tx1 | sed -re 's/[[:alnum:]]+/0x&,/g' -e 's/[[:space:]]+//g' > $(srcdir)/$@
+	$(call my_xa_recipe,-DSCREEN -DSMOOTH_SCROLL)

src/libpsid64/psidboot.h
@@ -17,5 +17,8 @@
 0x29,0x03,0xa0,0x73,0xb9,0x04,0x10,0x99,0xf7,0x00,0x88,0xd0,0xf7,0x4c,0x14,0x01,
 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
 0x00,0x6b,0x37,0x00,0x00,0x00,0x08,0x00,0x14,0x0f,0x00,0x00,0x00,0x00,0x00,0x00,
-0x00,0x06,0x01,0x02,0x03,0x04,0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,
-
+0x00,0x06,0x01,0x02,0x03,0x04,0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x04,0x00,
+0x73,0x6f,0x6e,0x67,0x00,0x70,0x6c,0x61,0x79,0x65,0x72,0x00,0x73,0x74,0x6f,0x70,
+0x76,0x65,0x63,0x00,0x43,0x4f,0x4c,0x5f,0x42,0x4f,0x52,0x44,0x45,0x52,0x00,0x02,
+0x20,0x00,0x00,0x02,0x82,0x73,0x80,0x01,0x00,0x1b,0x82,0x0d,0x20,0x02,0x00,0x05,
+0x40,0x02,0x00,0x00,0x07,0x82,0x2b,0x20,0x03,0x00,0x00,0x00,0x00,0x00,

src/libpsid64/psiddrv.h
@@ -33,5 +33,10 @@
 0xf7,0xef,0xef,0xef,0xdf,0xef,0xef,0xef,0xdf,0x7f,0xfb,0xfd,0xfb,0xf7,0xf7,0xfd,
 0xfb,0xf7,0xfd,0x80,0x01,0x08,0x01,0x08,0x01,0x08,0x01,0x08,0x01,0x08,0x04,0x10,
 0x10,0x04,0x40,0x20,0x04,0x20,0x02,0x04,0x20,0x04,0x10,0x80,0x40,0x02,0x40,0x02,
-0x20,0x40,0x40,0x80,0x02,0x80,0x02,0x10,
-
+0x20,0x40,0x40,0x80,0x02,0x80,0x02,0x10,0x01,0x00,0x73,0x69,0x64,0x32,0x62,0x61,
+0x73,0x65,0x00,0x02,0x82,0x03,0x82,0x13,0x82,0x02,0x82,0x02,0x82,0x0a,0x82,0x07,
+0x82,0x12,0x82,0x17,0x82,0x04,0x82,0x04,0x82,0x14,0x82,0x03,0x82,0x05,0x82,0x13,
+0x82,0x15,0x82,0x03,0x82,0x09,0x82,0x17,0x82,0x16,0x82,0x05,0x22,0x09,0x82,0x0b,
+0x82,0x08,0x82,0x03,0x82,0x05,0x82,0x0f,0x82,0x05,0x82,0x04,0x82,0x39,0x80,0x00,
+0x00,0x15,0x82,0x05,0x82,0x09,0x82,0x05,0x82,0x0a,0x82,0x09,0x82,0x08,0x82,0x04,
+0x82,0x06,0x82,0x05,0x82,0x00,0x00,0x00,0x00,

src/libpsid64/psidextboot.h
@@ -27,5 +27,32 @@
 0x00,0x00,0x00,0x00,0x00,0x00,0x6b,0x37,0x00,0x00,0x01,0x08,0x00,0x00,0x0f,0x00,
 0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x01,0x02,0x03,0x04,0x00,0x00,0x02,0x03,0x04,
 0x05,0x06,0x07,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
-0x00,0x00,
-
+0x00,0x00,0x1e,0x00,0x73,0x6f,0x6e,0x67,0x00,0x62,0x61,0x72,0x73,0x70,0x72,0x70,
+0x74,0x72,0x00,0x73,0x63,0x72,0x65,0x65,0x6e,0x00,0x70,0x6c,0x61,0x79,0x65,0x72,
+0x00,0x64,0x64,0x30,0x30,0x00,0x43,0x4f,0x4c,0x5f,0x42,0x41,0x43,0x4b,0x47,0x52,
+0x4f,0x55,0x4e,0x44,0x00,0x43,0x4f,0x4c,0x5f,0x54,0x49,0x54,0x4c,0x45,0x00,0x43,
+0x4f,0x4c,0x5f,0x56,0x41,0x4c,0x55,0x45,0x00,0x43,0x4f,0x4c,0x5f,0x50,0x41,0x52,
+0x41,0x4d,0x45,0x54,0x45,0x52,0x00,0x43,0x4f,0x4c,0x5f,0x43,0x4f,0x4c,0x4f,0x4e,
+0x00,0x43,0x4f,0x4c,0x5f,0x4c,0x45,0x47,0x45,0x4e,0x44,0x00,0x73,0x74,0x6f,0x70,
+0x76,0x65,0x63,0x00,0x64,0x30,0x31,0x38,0x00,0x43,0x4f,0x4c,0x5f,0x42,0x4f,0x52,
+0x44,0x45,0x52,0x00,0x43,0x4f,0x4c,0x5f,0x42,0x41,0x52,0x5f,0x46,0x47,0x00,0x43,
+0x4f,0x4c,0x5f,0x4c,0x49,0x4e,0x45,0x5f,0x30,0x00,0x43,0x4f,0x4c,0x5f,0x4c,0x49,
+0x4e,0x45,0x5f,0x31,0x00,0x43,0x4f,0x4c,0x5f,0x4c,0x49,0x4e,0x45,0x5f,0x32,0x00,
+0x43,0x4f,0x4c,0x5f,0x4c,0x49,0x4e,0x45,0x5f,0x33,0x00,0x43,0x4f,0x4c,0x5f,0x4c,
+0x49,0x4e,0x45,0x5f,0x34,0x00,0x43,0x4f,0x4c,0x5f,0x4c,0x49,0x4e,0x45,0x5f,0x35,
+0x00,0x43,0x4f,0x4c,0x5f,0x4c,0x49,0x4e,0x45,0x5f,0x36,0x00,0x43,0x4f,0x4c,0x5f,
+0x4c,0x49,0x4e,0x45,0x5f,0x37,0x00,0x43,0x4f,0x4c,0x5f,0x4c,0x49,0x4e,0x45,0x5f,
+0x38,0x00,0x43,0x4f,0x4c,0x5f,0x4c,0x49,0x4e,0x45,0x5f,0x39,0x00,0x43,0x4f,0x4c,
+0x5f,0x4c,0x49,0x4e,0x45,0x5f,0x31,0x30,0x00,0x43,0x4f,0x4c,0x5f,0x4c,0x49,0x4e,
+0x45,0x5f,0x31,0x31,0x00,0x43,0x4f,0x4c,0x5f,0x4c,0x49,0x4e,0x45,0x5f,0x31,0x32,
+0x00,0x43,0x4f,0x4c,0x5f,0x4c,0x49,0x4e,0x45,0x5f,0x31,0x33,0x00,0x43,0x4f,0x4c,
+0x5f,0x4c,0x49,0x4e,0x45,0x5f,0x31,0x34,0x00,0x02,0x20,0x00,0x00,0x02,0x82,0x9c,
+0x20,0x01,0x00,0x02,0x80,0x02,0x00,0x07,0x80,0x03,0x00,0x1b,0x82,0x09,0x20,0x04,
+0x00,0x05,0x20,0x05,0x00,0x13,0x20,0x06,0x00,0x0a,0x82,0x0d,0x20,0x07,0x00,0x08,
+0x20,0x08,0x00,0x03,0x20,0x09,0x00,0x0f,0x20,0x0a,0x00,0x0e,0x20,0x0b,0x00,0x05,
+0x40,0x0b,0x00,0x00,0x07,0x82,0x23,0x20,0x0c,0x00,0x08,0x20,0x0d,0x00,0x01,0x20,
+0x05,0x00,0x06,0x20,0x0e,0x00,0x07,0x20,0x0f,0x00,0x01,0x20,0x10,0x00,0x01,0x20,
+0x11,0x00,0x01,0x20,0x12,0x00,0x01,0x20,0x13,0x00,0x01,0x20,0x14,0x00,0x01,0x20,
+0x15,0x00,0x01,0x20,0x16,0x00,0x01,0x20,0x17,0x00,0x01,0x20,0x18,0x00,0x01,0x20,
+0x19,0x00,0x01,0x20,0x1a,0x00,0x01,0x20,0x1b,0x00,0x01,0x20,0x1c,0x00,0x01,0x20,
+0x1d,0x00,0x00,0x00,0x00,0x00,

src/libpsid64/psidextdrv.h
@@ -82,4 +82,61 @@
 0xbd,0x49,0x03,0x9d,0x48,0x03,0xe8,0xe0,0x27,0x90,0xf5,0xae,0xda,0x14,0xe0,0xff,
 0xd0,0x09,0xe8,0x8e,0xd7,0x14,0x8c,0xd8,0x14,0xf0,0xf0,0x8e,0x6f,0x03,0xee,0xd7,
 0x14,0xd0,0x03,0xee,0xd8,0x14,0x60,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
-
+0x24,0x00,0x43,0x4f,0x4c,0x5f,0x52,0x41,0x53,0x54,0x45,0x52,0x5f,0x54,0x49,0x4d,
+0x45,0x00,0x73,0x63,0x72,0x65,0x65,0x6e,0x5f,0x73,0x6f,0x6e,0x67,0x6e,0x75,0x6d,
+0x00,0x73,0x63,0x72,0x65,0x65,0x6e,0x00,0x73,0x6f,0x6e,0x67,0x6c,0x65,0x6e,0x67,
+0x74,0x68,0x73,0x5f,0x6d,0x69,0x6e,0x00,0x73,0x6f,0x6e,0x67,0x6c,0x65,0x6e,0x67,
+0x74,0x68,0x73,0x5f,0x73,0x65,0x63,0x00,0x73,0x6f,0x6e,0x67,0x74,0x70,0x69,0x5f,
+0x6c,0x6f,0x00,0x73,0x6f,0x6e,0x67,0x74,0x70,0x69,0x5f,0x68,0x69,0x00,0x73,0x69,
+0x64,0x32,0x62,0x61,0x73,0x65,0x00,0x73,0x69,0x64,0x33,0x62,0x61,0x73,0x65,0x00,
+0x43,0x4f,0x4c,0x5f,0x42,0x41,0x52,0x5f,0x46,0x47,0x00,0x43,0x4f,0x4c,0x5f,0x42,
+0x41,0x52,0x5f,0x42,0x47,0x00,0x43,0x4f,0x4c,0x5f,0x46,0x4f,0x4f,0x54,0x45,0x52,
+0x5f,0x30,0x00,0x43,0x4f,0x4c,0x5f,0x46,0x4f,0x4f,0x54,0x45,0x52,0x5f,0x31,0x00,
+0x43,0x4f,0x4c,0x5f,0x46,0x4f,0x4f,0x54,0x45,0x52,0x5f,0x32,0x00,0x43,0x4f,0x4c,
+0x5f,0x46,0x4f,0x4f,0x54,0x45,0x52,0x5f,0x33,0x00,0x43,0x4f,0x4c,0x5f,0x46,0x4f,
+0x4f,0x54,0x45,0x52,0x5f,0x34,0x00,0x43,0x4f,0x4c,0x5f,0x46,0x4f,0x4f,0x54,0x45,
+0x52,0x5f,0x35,0x00,0x43,0x4f,0x4c,0x5f,0x46,0x4f,0x4f,0x54,0x45,0x52,0x5f,0x36,
+0x00,0x43,0x4f,0x4c,0x5f,0x46,0x4f,0x4f,0x54,0x45,0x52,0x5f,0x37,0x00,0x43,0x4f,
+0x4c,0x5f,0x46,0x4f,0x4f,0x54,0x45,0x52,0x5f,0x38,0x00,0x43,0x4f,0x4c,0x5f,0x46,
+0x4f,0x4f,0x54,0x45,0x52,0x5f,0x39,0x00,0x43,0x4f,0x4c,0x5f,0x46,0x4f,0x4f,0x54,
+0x45,0x52,0x5f,0x31,0x30,0x00,0x43,0x4f,0x4c,0x5f,0x46,0x4f,0x4f,0x54,0x45,0x52,
+0x5f,0x31,0x31,0x00,0x43,0x4f,0x4c,0x5f,0x46,0x4f,0x4f,0x54,0x45,0x52,0x5f,0x31,
+0x32,0x00,0x43,0x4f,0x4c,0x5f,0x46,0x4f,0x4f,0x54,0x45,0x52,0x5f,0x31,0x33,0x00,
+0x43,0x4f,0x4c,0x5f,0x46,0x4f,0x4f,0x54,0x45,0x52,0x5f,0x31,0x34,0x00,0x43,0x4f,
+0x4c,0x5f,0x46,0x4f,0x4f,0x54,0x45,0x52,0x5f,0x31,0x35,0x00,0x73,0x74,0x69,0x6c,
+0x00,0x43,0x4f,0x4c,0x5f,0x53,0x43,0x52,0x4f,0x4c,0x4c,0x45,0x52,0x5f,0x30,0x00,
+0x43,0x4f,0x4c,0x5f,0x53,0x43,0x52,0x4f,0x4c,0x4c,0x45,0x52,0x5f,0x31,0x00,0x43,
+0x4f,0x4c,0x5f,0x53,0x43,0x52,0x4f,0x4c,0x4c,0x45,0x52,0x5f,0x32,0x00,0x43,0x4f,
+0x4c,0x5f,0x53,0x43,0x52,0x4f,0x4c,0x4c,0x45,0x52,0x5f,0x33,0x00,0x43,0x4f,0x4c,
+0x5f,0x53,0x43,0x52,0x4f,0x4c,0x4c,0x45,0x52,0x5f,0x34,0x00,0x43,0x4f,0x4c,0x5f,
+0x53,0x43,0x52,0x4f,0x4c,0x4c,0x45,0x52,0x5f,0x35,0x00,0x43,0x4f,0x4c,0x5f,0x53,
+0x43,0x52,0x4f,0x4c,0x4c,0x45,0x52,0x5f,0x36,0x00,0x43,0x4f,0x4c,0x5f,0x53,0x43,
+0x52,0x4f,0x4c,0x4c,0x45,0x52,0x5f,0x37,0x00,0x02,0x82,0x03,0x82,0x20,0x82,0x02,
+0x82,0x02,0x82,0x15,0x20,0x00,0x00,0x08,0x82,0x07,0x82,0x21,0x82,0x03,0x82,0x20,
+0x82,0x04,0x82,0x04,0x82,0x14,0x82,0x03,0x40,0x01,0x00,0x00,0x04,0x82,0x14,0x80,
+0x01,0x00,0x15,0x80,0x01,0x00,0x07,0x80,0x01,0x00,0x05,0x80,0x01,0x00,0x0e,0x82,
+0x03,0x80,0x02,0x00,0x06,0x82,0x03,0x82,0x03,0x82,0x03,0x82,0x03,0x82,0x03,0x82,
+0x05,0x82,0x09,0x82,0x0b,0x82,0x15,0x82,0x03,0x82,0x09,0x40,0x03,0x00,0x00,0x04,
+0x80,0x03,0x00,0x05,0x82,0x03,0x80,0x04,0x00,0x04,0x82,0x03,0x80,0x05,0x00,0x03,
+0x82,0x03,0x80,0x06,0x00,0x03,0x82,0x03,0x82,0x17,0x82,0x16,0x82,0x05,0x22,0x09,
+0x82,0x0b,0x82,0x08,0x82,0x03,0x82,0x05,0x82,0x0f,0x82,0x05,0x82,0x03,0x82,0x1a,
+0x82,0x0e,0x82,0x07,0x82,0x03,0x82,0x03,0x82,0x03,0x82,0x03,0x82,0x2f,0x80,0x07,
+0x00,0x03,0x80,0x08,0x00,0x12,0x80,0x07,0x00,0x03,0x80,0x08,0x00,0x06,0x82,0x08,
+0x80,0x07,0x00,0x03,0x80,0x08,0x00,0x04,0x82,0x08,0x82,0x1a,0x82,0x05,0x82,0x09,
+0x82,0x05,0x82,0x0d,0x82,0x09,0x82,0x08,0x82,0x04,0x82,0x06,0x82,0x06,0x82,0x05,
+0x82,0x0a,0x82,0x04,0x82,0x0c,0x82,0x05,0x82,0x04,0x82,0x05,0x82,0x5d,0x82,0x03,
+0x82,0x03,0x82,0x06,0x82,0x04,0x82,0x0c,0x82,0x05,0x82,0x05,0x82,0x06,0x82,0x06,
+0x82,0x04,0x82,0x07,0x82,0x03,0x82,0x07,0x82,0x03,0x80,0x02,0x00,0x05,0x82,0x0a,
+0x80,0x02,0x00,0x06,0x82,0x03,0x82,0x03,0x82,0x03,0x82,0x04,0x82,0x05,0x82,0x05,
+0x82,0x03,0x82,0x05,0x82,0x03,0x82,0x05,0x82,0x03,0x82,0x03,0x82,0x07,0x82,0x06,
+0x82,0x11,0x82,0x08,0x80,0x02,0x00,0x05,0x82,0x16,0x80,0x02,0x00,0x05,0x80,0x02,
+0x00,0x0d,0x20,0x09,0x00,0x05,0x20,0x0a,0x00,0x3c,0x82,0x03,0x82,0x07,0x82,0x0c,
+0x20,0x0b,0x00,0x01,0x20,0x0c,0x00,0x01,0x20,0x0d,0x00,0x01,0x20,0x0e,0x00,0x01,
+0x20,0x0f,0x00,0x01,0x20,0x10,0x00,0x01,0x20,0x11,0x00,0x01,0x20,0x12,0x00,0x01,
+0x20,0x13,0x00,0x01,0x20,0x14,0x00,0x01,0x20,0x15,0x00,0x01,0x20,0x16,0x00,0x01,
+0x20,0x17,0x00,0x01,0x20,0x18,0x00,0x01,0x20,0x19,0x00,0x01,0x20,0x1a,0x00,0x02,
+0x40,0x1b,0x00,0x00,0x0f,0x82,0x03,0x82,0x07,0x82,0x03,0x82,0x03,0x82,0x06,0x82,
+0x08,0x80,0x02,0x00,0x03,0x80,0x02,0x00,0x08,0x82,0x08,0x82,0x03,0x82,0x05,0x80,
+0x02,0x00,0x03,0x82,0x05,0x82,0x04,0x20,0x1c,0x00,0x01,0x20,0x1d,0x00,0x01,0x20,
+0x1e,0x00,0x01,0x20,0x1f,0x00,0x01,0x20,0x20,0x00,0x01,0x20,0x21,0x00,0x01,0x20,
+0x22,0x00,0x01,0x20,0x23,0x00,0x00,0x00,0x00,0x00,